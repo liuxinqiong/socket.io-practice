1f0c0be268bb90970290add0207aa0f8de350416
liuxinqiong <372513646@qq.com>
2017-08-05 16:14:03 +0800

    fix bug in pos

--- server/posServer.js
+++ server/posServer.js
@@ -24,15 +24,14 @@ module.exports=function(io){
                 userId:data.userId,
                 socketId:socket.id
             };
+            // 新用户连接，告诉其他人我上线啦
+            socket.broadcast.emit('onLineList',Object.keys(users));
         });
 
-        // 新用户连接，发布在线列表
-        ioPos.emit('onLineList',Object.keys(users));
-
-        // 管理员才发布，获取在线列表
+        // 管理员才发布获取在线列表请求
         socket.on('getOnlineListReq',function(){
             console.log('pos loginPos');
-            ioPos.emit('onLineList',Object.keys(users));
+            socket.emit('onLineList',Object.keys(users));
         });
 
         // 根据用户id，找到指定socket，然后发布请求
@@ -42,31 +41,45 @@ module.exports=function(io){
             if(user){
                 // 发布请求，监听响应
                 var socketId=user.socketId;
-                ioPos.sockets[socketId].emit('getPosReq');
-                ioPos.sockets[socketId].on('getPosRes',function (_data) {
-                    // 定位权限未打开
-                    if(_data.code===0){
-                        // 定位成功
-                        socket.emit('posResult', {
-                            resultCode: 0,
-                            resultInfo: '定位成功',
-                            resultData: {
-                                position: _data.position,
-                                userId:data.userId
-                            }
-                        });
-                    }else{
-                        // 定位失败，定位出错或定位功能未开启
-                        socket.emit('posResult', {
-                            resultCode: -2,
-                            resultInfo: '定位出错，用户定位功能可能未开启',
-                            resultData: {
-                                position: null,
-                                userId:data.userId
-                            }
-                        });
-                    }
-                });
+                var targetSocket=ioPos.sockets[socketId];
+                if(targetSocket){
+                    // disconnect事件可能没触发，但是socket已经不存在
+                    targetSocket.emit('getPosReq');
+                    targetSocket.on('getPosRes',function (_data) {
+                        // 定位权限未打开
+                        if(_data.code===0){
+                            // 定位成功
+                            socket.emit('posResult', {
+                                resultCode: 0,
+                                resultInfo: '定位成功',
+                                resultData: {
+                                    position: _data.position,
+                                    userId:data.userId
+                                }
+                            });
+                        }else{
+                            // 定位失败，定位出错或定位功能未开启
+                            socket.emit('posResult', {
+                                resultCode: -2,
+                                resultInfo: '定位出错，用户定位功能可能未开启',
+                                resultData: {
+                                    position: null,
+                                    userId:data.userId
+                                }
+                            });
+                        }
+                    });
+                }else{
+                    // 不存在
+                    socket.emit('posResult',{
+                        resultCode:-1,
+                        resultInfo:'用户不在线',
+                        resultData:{
+                            userId:data.userId,
+                            position: null
+                        }
+                    });
+                }
             }else{
                 // 不存在
                 socket.emit('posResult',{
@@ -80,10 +93,11 @@ module.exports=function(io){
             }
         });
 
-        socket.on('disconnection',function(){
+        socket.on('disconnect',function(){
+            console.log('disconnect');
             // 删除离线用户
             deleteUserBySocketId(socket.id);
-            // 发布最新在线列表，管理员才监听，因此全部广播
+            // 用户断开连接，通知其他人有人离线了
             ioPos.emit('onLineList',Object.keys(users));
         });
     })
